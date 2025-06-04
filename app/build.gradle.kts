efcc3b380a4617b4d100c664fd61a9e9dbea0ffb
Andres Moreno <andres.moreno04@usa.edu.co>
2025-06-03 22:47:03 -0500

    Arreglo Oauth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.cardiovascularmodel.livingheart"
+        applicationId = "com.cardiovascularmodel.livingheart2"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
